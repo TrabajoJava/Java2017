6fff911d3a039aed0b53ff27d2416189e3c24be5
Bianca <Bianca@DESKTOP-1NHA0OJ>
2017-07-26 18:32:26 -0300

    El add esta por la mitad

--- src/data/DataPersona.java
+++ src/data/DataPersona.java
@@ -47,16 +47,44 @@ public class DataPersona {
 		ResultSet rs = null;
 		Persona p = null;
 		
-		stmt = FactoryConexion.getInstancia().getConn().prepareStatement("select id, nombre, apellido, dni, habilitado from persona where dni=?");
-		stmt.setString(1, per.getDni());
-		rs = stmt.executeQuery();
-		if(rs!=null && ){
+		try {
+			stmt = FactoryConexion.getInstancia().getConn().prepareStatement("select id, nombre, apellido, dni, habilitado from persona where dni=?");
+			stmt.setString(1, per.getDni());
+			rs = stmt.executeQuery();
+			if(rs!=null && rs.next()){
+				p = new Persona();
+				p.setId(rs.getInt("id"));
+				p.setApellido(rs.getString("apellido"));
+				p.setNombre(rs.getString("nombre"));
+				p.setHabilitado(rs.getBoolean("habilitado"));
+				p.setDni(rs.getString("dni"));
+				
+										}
+		} catch (SQLException e) {
 			
-			
+			e.printStackTrace();
 		}
+		
+			try {
+				if(rs!=null) rs.close();
+				if(stmt!=null)stmt.close();
+				FactoryConexion.getInstancia().releaseConn();
+			} catch (SQLException e) {
+			
+				e.printStackTrace();
+			}
+			return p;
 }
 			
-		
+		public void add(Persona per){
+				PreparedStatement stmt = null;
+				ResultSet rs = null;
+				Persona p = null;
+				
+				stmt = FactoryConexion.getInstancia().getConn().prepareStatement("insert into persona(nombre,apellido,dni,habilitado) values(?,?,?,?)");
+				//REvisar que esten en ese roden en la tabla
+				
+		}
 		
 		
 		
