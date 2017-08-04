473df40a4dfaeda1df6be0babe6d2f57294943b0
Bianca <Bianca@DESKTOP-1NHA0OJ>
2017-08-04 11:19:40 -0300

    terminamos data persona y agregamos metodos modificar y borrar

--- src/data/DataPersona.java
+++ src/data/DataPersona.java
@@ -42,14 +42,14 @@ public class DataPersona {
 			return pers;		
 			}
 	
-	public Persona getByDni(Persona per){
+	public Persona getByDni(String docu){
 		PreparedStatement stmt=null;
 		ResultSet rs = null;
 		Persona p = null;
 		
 		try {
 			stmt = FactoryConexion.getInstancia().getConn().prepareStatement("select id, nombre, apellido, dni, habilitado from persona where dni=?");
-			stmt.setString(1, per.getDni());
+			stmt.setString(1, docu);
 			rs = stmt.executeQuery();
 			if(rs!=null && rs.next()){
 				p = new Persona();
@@ -81,24 +81,90 @@ public class DataPersona {
 				ResultSet keyResultSet=null;
 
 				
-				stmt = FactoryConexion.getInstancia().getConn().prepareStatement("insert into persona(nombre,apellido,dni,habilitado) values(?,?,?,?)",
-						PreparedStatement.RETURN_GENERATED_KEYS);
-				/*REvisar que esten en ese roden en la tabla*/
-				stmt.setString(1, per.getNombre());
-				stmt.setString(2, per.getApellido());
-				stmt.setString(3, per.getDni());
-				stmt.setBoolean(4, per.isHabilitado());
+				try {
+					stmt = FactoryConexion.getInstancia().getConn().prepareStatement("insert into persona(nombre,apellido,dni,habilitado) values(?,?,?,?)",
+							PreparedStatement.RETURN_GENERATED_KEYS);
+					/*REvisar que esten en ese roden en la tabla*/
+					stmt.setString(1, per.getNombre());
+					stmt.setString(2, per.getApellido());
+					stmt.setString(3, per.getDni());
+					stmt.setBoolean(4, per.isHabilitado());
+					stmt.executeUpdate();
+					keyResultSet=stmt.getGeneratedKeys();
+					if(keyResultSet!=null && keyResultSet.next()){
+						per.setId(keyResultSet.getInt(1));  
+					}
+					
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				try {
+					if(keyResultSet!=null)keyResultSet.close();
+					if(stmt!=null)stmt.close();
+					FactoryConexion.getInstancia().releaseConn();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				
+		}
+		
+		
+		public void deleteByDni(String docu){
+			
+			PreparedStatement stmt = null;
+			
+			
+			try {
+				stmt =  FactoryConexion.getInstancia().getConn().prepareStatement("delete from persona where dni=?");
+				stmt.setString(1, docu);
 				stmt.executeUpdate();
 				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 				
-		}
-		
-		
-		
-		
-		
-	}
+			try {
+				if(stmt!=null)stmt.close();
+				FactoryConexion.getInstancia().releaseConn();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 
 		}
+
+		public void update(Persona per){
+			
+			PreparedStatement stmt = null;
+			
+			
+			try {
+				stmt = FactoryConexion.getInstancia().getConn().prepareStatement("update persona set dni = ?, nombre = ?, apellido = ?, habilitado = ? where dni = ?");
+				stmt.setString(1, per.getDni());
+				stmt.setString(2, per.getNombre());
+				stmt.setString(3, per.getApellido());
+				stmt.setBoolean(4, per.isHabilitado());
+				stmt.setString(5, per.getDni());
+				stmt.executeUpdate();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				if(stmt!=null)stmt.close();
+				FactoryConexion.getInstancia().releaseConn();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}		
+}			
+
 	
 
