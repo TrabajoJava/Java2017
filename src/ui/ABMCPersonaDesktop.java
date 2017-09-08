70456e99331791c1139fc7d2c99c1a4aa36c7eee
Facundo <Facundo@DESKTOP-FNP6CAV>
2017-09-08 18:32:39 -0300

    Borramos un ABM que no iba, agregamos usuario y contraseña
    Agregamos ComboBox de categoria

--- src/ui/ABMCPersonaDesktop.java
+++ src/ui/ABMCPersonaDesktop.java
@@ -20,6 +20,9 @@ import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ABMCPersonaDesktop extends JInternalFrame {
 
@@ -34,6 +37,14 @@ public class ABMCPersonaDesktop extends JInternalFrame {
 	private JButton btnBorrar;
 	private JButton btnModificar;
 	private JTextField txtId;
+	private JLabel lblCategoria;
+	private JComboBox cbCategoria;
+	private JTextField txtCategoria;
+	private JLabel lblUsuario;
+	private JLabel lblConstrasena;
+	private JTextField txtUsuario;
+	private JTextField txtContrasena;
+
 
 	/**
 	 * Launch the application.
@@ -57,7 +68,7 @@ public class ABMCPersonaDesktop extends JInternalFrame {
 	public ABMCPersonaDesktop() {
 		setClosable(true);
 		setDefaultCloseOperation(JInternalFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 328, 300);
+		setBounds(100, 100, 451, 358);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -79,6 +90,16 @@ public class ABMCPersonaDesktop extends JInternalFrame {
 		
 		chkHabilitado = new JCheckBox("Habilitado");
 		chkHabilitado.setSelected(true);
+		
+		lblUsuario = new JLabel("Usuario");
+		
+		lblConstrasena = new JLabel("Contrasena");
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setColumns(10);
+		
+		txtContrasena = new JTextField();
+		txtContrasena.setColumns(10);
 		
 		JButton btnBuscar = new JButton("Buscar");
 		btnBuscar.addMouseListener(new MouseAdapter() {
@@ -117,40 +138,65 @@ public class ABMCPersonaDesktop extends JInternalFrame {
 		txtId = new JTextField();
 		txtId.setEditable(false);
 		txtId.setColumns(10);
+		
+		txtCategoria = new JTextField();
+		txtCategoria.setColumns(20);
+		
+		lblCategoria = new JLabel("Categoria");
+		
+		cbCategoria = new JComboBox();
+		
+		cbCategoria.addItem("0-Usuario");
+		cbCategoria.addItem("1-Encargado");
+		cbCategoria.addItem("2-Administrador");
+		
+		cbCategoria.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				txtCategoria.setText(cbCategoria.getSelectedItem().toString());
+			}
+		});
+		
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(chkHabilitado)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblApellido)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(txtApellido, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addComponent(btnAgregar)
-							.addGap(18)
+							.addGap(35)
 							.addComponent(btnBorrar)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(btnModificar))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-								.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
-									.addComponent(lblId)
-									.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addComponent(txtId, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-								.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
-									.addComponent(lblDni)
-									.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addComponent(txtDni, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-								.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
-									.addComponent(lblNombre)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-							.addGap(36)
-							.addComponent(btnBuscar)))
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+							.addGap(37)
+							.addComponent(btnModificar)
+							.addGap(32)
+							.addComponent(btnBuscar))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblApellido)
+								.addComponent(lblId)
+								.addComponent(lblDni)
+								.addComponent(lblNombre)
+								.addComponent(lblConstrasena)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+									.addComponent(lblUsuario, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(lblCategoria, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+							.addPreferredGap(ComponentPlacement.RELATED, 98, Short.MAX_VALUE)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+									.addComponent(txtNombre)
+									.addComponent(txtDni)
+									.addComponent(txtApellido, Alignment.TRAILING)
+									.addComponent(cbCategoria, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(txtUsuario)
+									.addComponent(txtContrasena))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(txtId, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED, 165, Short.MAX_VALUE)))
+							.addGap(30))
+						.addComponent(chkHabilitado))
+					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -162,7 +208,6 @@ public class ABMCPersonaDesktop extends JInternalFrame {
 					.addGap(11)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblDni)
-						.addComponent(btnBuscar)
 						.addComponent(txtDni, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
@@ -172,14 +217,27 @@ public class ABMCPersonaDesktop extends JInternalFrame {
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblApellido)
 						.addComponent(txtApellido, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(11)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblCategoria)
+						.addComponent(cbCategoria, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblUsuario)
+						.addComponent(txtUsuario, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblConstrasena)
+						.addComponent(txtContrasena, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(chkHabilitado)
-					.addGap(32)
+					.addGap(5)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnAgregar)
 						.addComponent(btnBorrar)
-						.addComponent(btnModificar))
-					.addContainerGap(23, Short.MAX_VALUE))
+						.addComponent(btnModificar)
+						.addComponent(btnBuscar))
+					.addGap(19))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
@@ -211,16 +269,4 @@ public class ABMCPersonaDesktop extends JInternalFrame {
 		this.chkHabilitado.setSelected(p.isHabilitado());
 		this.txtId.setText(String.valueOf(p.getId()));
 	}
-	
-	/*private Persona mapearDeForm(){			Hay que mandarle que tipo de persona es
-		Persona p=new Persona();
-		if(!this.txtId.getText().isEmpty()){
-			p.setId(Integer.parseInt(this.txtId.getText()));
-		}
-		p.setDni(this.txtDni.getText());
-		p.setNombre(this.txtNombre.getText());
-		p.setApellido(this.txtApellido.getText());
-		p.setHabilitado(this.chkHabilitado.isSelected());
-		return p;
-	}*/
 }
