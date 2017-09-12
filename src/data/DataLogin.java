eefb58ffcfc605fbcfdfd96954b2fe44d113ed0d
juan helbert <helbert.juan@gmail.com>
2017-09-12 20:36:59 -0300

    Cosas con Login

--- src/data/DataLogin.java
+++ src/data/DataLogin.java
@@ -1,5 +1,50 @@
 package data;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import entity.Persona;
+import ui.Login;
+
 public class DataLogin {
+	
+	public Persona compara(Persona per){
+		Persona p = null;
+		ResultSet rs = null;
+		PreparedStatement stmt = null;
+		
+		try{
+			stmt = FactoryConexion.getInstancia().getConn().prepareStatement(
+					"select id, nombre, apellido, dni, habilitado, user, pass FROM personasTP where user=? and pass = ?");
+		stmt.setString(1, per.getUsuario());
+		stmt.setString(2, per.getContrasena());
+		rs = stmt.executeQuery();
+			if (rs != null && rs.next()){
+				p = new Persona();
+				p.setId(rs.getInt("id"));
+				p.setApellido(rs.getString("apellido"));
+				p.setNombre(rs.getString("nombre"));
+				p.setDni(rs.getString("dni"));
+				p.setHabilitado(rs.getBoolean("habilitado"));
+				p.setUsuario(rs.getString("user"));
+				p.setContrasena(rs.getString("pass"));
+			}
+		} catch (SQLException e){
+			e.printStackTrace();
+			}
+		
+		try{
+			if(rs != null) rs.close();
+			if(stmt != null) stmt.close();
+			FactoryConexion.getInstancia().releaseConn();
+		} catch (SQLException e){
+			
+			e.printStackTrace();
+		}
+		
+		return p;
+	}
+
 
 }
