d49fe59a4991e0257b3935dec1efdfa0fee8ab9e
juan helbert <helbert.juan@gmail.com>
2017-09-12 16:55:45 -0300

    Minimo cambio

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -8,30 +8,24 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 
-public class menu extends JFrame {
+public class Menu extends JFrame {
 
 	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
 	 */
-/*	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					menu frame = new menu();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}*/
+	/*
+	 * public static void main(String[] args) { EventQueue.invokeLater(new
+	 * Runnable() { public void run() { try { menu frame = new menu();
+	 * frame.setVisible(true); } catch (Exception e) { e.printStackTrace(); } }
+	 * }); }
+	 */
 
 	/**
 	 * Create the frame.
 	 */
-	public menu() {
+	public Menu() {
 		setTitle("Menu");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -39,19 +33,19 @@ public class menu extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton btnNewButton = new JButton("ABMCElemento");
 		btnNewButton.setBounds(165, 78, 140, 23);
 		contentPane.add(btnNewButton);
-		
+
 		JButton btnAbmctipoelemento = new JButton("ABMCTipoElemento");
 		btnAbmctipoelemento.setBounds(165, 131, 140, 23);
 		contentPane.add(btnAbmctipoelemento);
-		
+
 		JButton btnAbmcpersona = new JButton("ABMCPersona");
 		btnAbmcpersona.setBounds(165, 27, 140, 23);
 		contentPane.add(btnAbmcpersona);
-		
+
 		JButton btnNewButton_1 = new JButton("ABMCReserva");
 		btnNewButton_1.setBounds(165, 186, 140, 23);
 		contentPane.add(btnNewButton_1);
